a89c367ed47d0cdc0eb7cea40b5e909082da78bc
takurinton <takurinton@takurinton.dev>
2022-08-14 19:19:29 +0900

    header

--- public/header.tsx
+++ public/header.tsx
@@ -3,8 +3,16 @@ import { h } from "preact";
 export default function Header() {
   return (
     <header>
+      <a className="title" href="/">
+        blog.takurinton.dev
+      </a>
       <nav>
-        <a href="/">blog.takurinton.dev</a>
+        <a className="sub" href="https://takurinton.dev" target="_blank">
+          portfolio
+        </a>
+        <a className="sub" href="https://dev.takurinton.com" target="_blank">
+          memo
+        </a>
       </nav>
     </header>
   );
