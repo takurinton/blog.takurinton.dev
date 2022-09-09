7b28a1ee447e2dfa036e646da5bd914cc8b4e626
takurinton <takurinton@takurinton.dev>
2022-09-10 06:54:14 +0900

    ...

--- public/pages/post/index.js
+++ public/pages/post/index.js
@@ -1,6 +1,5 @@
 import { useRoute } from "preact-iso";
 import styles from "./style.module.css";
-import { MarkdownInit, markdown } from "src/md";
 import { useEffect } from "preact/hooks";
 import { usePost } from "src/hooks/usePost";
 import { setupHighlightjs } from "src/utils/setupHighlightjs";
@@ -8,13 +7,7 @@ import { useMetas } from "src/hooks/useMetas";
 
 export default function Post() {
   const { params } = useRoute();
-  const { mdStr } = usePost(params.id);
-  const md = new MarkdownInit(mdStr);
-
-  const title = md.getTitle();
-  const content = markdown(md.getContent());
-  const createdAt = md.getCreatedAt();
-  const description = `${title} について書きました。`;
+  const { title, description, createdAt, content } = usePost(params.id);
   useMetas({ title, description });
 
   useEffect(() => {
--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -1,27 +1,63 @@
 import { useState } from "preact/hooks";
+import { markdown, MarkdownInit } from "src/md";
+
+type FetchPost = {
+  mdStr: string;
+  meta: {};
+};
+
+type Post = {
+  id: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  content: string;
+};
+
+type Response = Post | Error;
 
 const CACHE = new Map();
 
-async function fetchPost(url) {
+async function fetchPost(url?: string): Promise<FetchPost> {
   const mdStr = await fetch(url).then((res) => res.text());
-  let meta = {};
+  const meta = {};
   return { mdStr, meta };
 }
 
-export function usePost(id: string): { mdStr: string; meta: any } {
+export function usePost(id: string): Response {
   const url = `/contents/${id}.md`;
-  let update = useState(0)[1];
+  const [, setPost] = useState(0);
   let post = CACHE.get(url);
-  if (!post) {
+  if (post === undefined) {
     post = fetchPost(url);
     CACHE.set(url, post);
     post.then(
-      (value) => update((post.value = value)),
-      (error) => update((post.error = error))
+      (value: FetchPost) => {
+        post.value = value;
+        setPost(post);
+      },
+      (error: any) => {
+        post.error = error;
+        setPost(post);
+      }
     );
   }
 
-  if (post.value) return post.value;
-  if (post.error) throw post.error;
+  if (post.value !== undefined) {
+    const md = new MarkdownInit(post.value.mdStr);
+    const title = md.getTitle();
+    const content = markdown(md.getContent());
+    const createdAt = md.getCreatedAt();
+    const description = `${title} について書きました。`;
+
+    return {
+      id,
+      title,
+      description,
+      createdAt,
+      content,
+    };
+  }
+  if (post.error !== undefined) throw new Error(post.error);
   throw post;
 }
