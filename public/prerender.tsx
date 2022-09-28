07eae1825108da619448c08f8b11f09d20d0ca67
takurinton <takurinton@takurinton.dev>
2022-09-28 16:42:36 +0900

    update: wmr@3.8.0

--- public/prerender.tsx
+++ public/prerender.tsx
@@ -2,23 +2,7 @@ import { prerender as ssr } from "preact-iso";
 import { VNode } from "preact";
 import { toStatic } from "hoofd/preact";
 
-let initialized = false;
-async function init() {
-  const fs = (await eval("u=>import(u)")("fs")).promises;
-  // @ts-ignore
-  globalThis.fetch = async (url) => {
-    const text = () =>
-      fs.readFile(`dist/${String(url).replace(/^\//, "")}`, "utf-8");
-    return { text, json: () => text().then(JSON.parse) };
-  };
-}
-
 export async function prerender(vnode: VNode) {
-  if (!initialized) {
-    initialized = true;
-    await init();
-  }
-
   const res = await ssr(vnode);
 
   const head = toStatic();
