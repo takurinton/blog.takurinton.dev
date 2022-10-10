eb547f897e81e9437fd73a6933c87297e41e93e9
takurinton <takurinton@takurinton.dev>
2022-10-10 09:49:56 +0900

    add: twitter renderer

--- src/md/markdown.ts
+++ src/md/markdown.ts
@@ -1,22 +1,27 @@
 import { marked } from "marked";
 
-export const markdownStyle = () => {
-  const r = new marked.Renderer();
-  r.heading = (text: string, level: number) => {
+const renderer = {
+  twitter(type, raw, text) {
+    const tweetId = text.split("/").pop();
+    return `
+      <blockquote class="twitter-tweet" id="${tweetId}"></blockquote>
+    `;
+  },
+  heading(text, level) {
     if (level === 1) {
       return `<h${level} class="h${level}" style="border-bottom: 2px solid #ff69b4; padding-bottom: 5px">${text}</h${level}>`;
     }
     return `<h${level} class="h${level}">${text}</h${level}>`;
-  };
-  r.table = (header: string, body: string) => {
+  },
+  table(header, body) {
     return `
       <table class="table" border="1" width="100%">
         ${header}
         ${body}
       </table>
     `;
-  };
-  r.list = (body: string, ordered: boolean, start: number) => {
+  },
+  list(body: string, ordered: boolean, start: number) {
     if (ordered) {
       return `
         <ul style='padding-left: ${start}'>
@@ -29,32 +34,41 @@ export const markdownStyle = () => {
           ${body}
       </ul>
     `;
-  };
-  r.image = (href: string | null, title: string | null, text: string) => {
+  },
+  image(href, title, text) {
     return `
       <img src=${href} alt=${text} class="content-img ${title ?? ""}" />
     `;
-  };
-  r.paragraph = (text: string) => {
+  },
+  paragraph(text) {
     return `
       <p class="p">${text}</p>
     `;
-  };
-  r.link = (href: string, title: string, text: string) => {
+  },
+  link(href, title, text) {
+    const isTwitterLink = /^https:\/\/twitter\.com\/\w+\/status\/\d+/.test(
+      href
+    );
+
+    if (isTwitterLink) {
+      const tweetId = href.split("/").pop();
+      return `
+            <blockquote class="twitter-tweet" id="${tweetId}"></blockquote>
+          `;
+    }
+
     return `
       <a href="${href}" target="_blank" class=${title}>${text}</a>
-      <x-link link="${href}"></x-link>
     `;
-  };
-  r.blockquote = (quote: string) => {
+  },
+  blockquote(quote) {
     return `<blockquote style="border-left:3px solid gray;margin:0 0 0 10px;padding-left:20px;color:gray;">${quote}</blockquote>`;
-  };
-  return r;
+  },
 };
 
 // TODO: rintonmd
 // https://github.com/takurinton/rintonmd
 export const markdown = (md: string): string => {
-  const renderer: marked.Renderer = markdownStyle();
-  return marked(md, { renderer });
+  marked.use({ renderer });
+  return marked.parse(md);
 };
---
+++ src/utils/setupTwitter.ts
@@ -0,0 +1,21 @@
+export const setupTwitter = async () => {
+  const script = document.createElement("script");
+  script.innerHTML = `
+    window.twttr = (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+      if (d.getElementById(id)) return t;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    
+      t._e = [];
+      t.ready = function(f) {
+        t._e.push(f);
+      };
+    
+      return t;
+    }(document, "script", "twitter-wjs"));`;
+  document.body.appendChild(script);
+};
