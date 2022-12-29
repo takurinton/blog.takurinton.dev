0740cfec469e9eaeb46e9040d410e8d767b5df32
takurinton <takurinton@takurinton.dev>
2022-12-29 09:12:14 +0900

    add: style tag when external link in content

--- public/pages/post/index.js
+++ public/pages/post/index.js
@@ -30,6 +30,62 @@ export default function Post() {
     }
   }, []);
 
+  useEffect(() => {
+    if (/<div class="og" /.test(content)) {
+      if (!window.getElementById("og-style")) {
+        const style = document.createElement("style");
+        Object.assign(style, {
+          id: "og-style",
+          innerHTML: `
+          .og > a {
+            border: 1px gray solid;
+            border-radius: 5px;
+            width: 80%;
+            padding: 10px;
+            display: flex;
+            text-decoration: none;
+            color: #222222;
+          }
+          .left {
+              height: 100px;
+              width: 100px;
+              text-align: center;
+              padding-right: 40px;
+          }
+          .left > img {
+              height: 100px;
+              width: 100px;
+          }
+          .right {
+              display: block;
+              overflow: hidden;
+          }
+          .right > h1,
+          .right > p,
+          .right > a {
+              overflow: hidden;
+              text-overflow: ellipsis;
+              white-space: nowrap;
+              text-overflow: ellipsis;
+          }
+          .right > h1 {
+              height: 50px;
+              margin: 0;
+          }
+          .right > p {
+              margin: 0;
+          }
+          .link { 
+              color: gray;
+          }
+          `,
+        });
+
+        document.head.appendChild(style);
+      }
+    }
+  }, []);
+
   return (
     <div class={styles.content}>
       <h1 class={styles.title}>{title}</h1>
