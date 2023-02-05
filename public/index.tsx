f62734df92de75c8e78996703b7df8e01f67fe16
takurinton <takurinton@takurinton.dev>
2023-02-05 11:27:30 +0900

    description

--- public/index.tsx
+++ public/index.tsx
@@ -1,10 +1,4 @@
-import {
-  LocationProvider,
-  Router,
-  // lazy,
-  ErrorBoundary,
-  hydrate,
-} from "preact-iso";
+import { LocationProvider, Router, ErrorBoundary, hydrate } from "preact-iso";
 import NotFound from "./pages/_404";
 import Header from "./header";
 import Home from "./pages/home";
@@ -13,23 +7,13 @@ import Post from "./pages/post";
 // eslint-disable-next-line no-unused-vars
 import { h } from "preact";
 
-// const Home = lazy(() => import("./pages/home"));
-// const Post = lazy(() => import("./pages/post"));
-
-function showLoading() {
-  document.body.classList.add("loading");
-}
-function hideLoading() {
-  document.body.classList.remove("loading");
-}
-
 export function App() {
   return (
     <LocationProvider>
       <div class="app">
         <Header />
         <ErrorBoundary>
-          <Router onLoadStart={showLoading} onLoadEnd={hideLoading}>
+          <Router>
             <Home path="/" />
             <Post path="/post/:id" />
             <NotFound default />
