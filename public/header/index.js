675b83f7709096298bc60a704b70767923bb0936
takurinton <takurinton@takurinton.dev>
2023-02-05 11:05:55 +0900

    fix: token

--- public/header/index.js
+++ public/header/index.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import { h } from "preact";
+import styles from "./style.module.css";
 
 export default function Header() {
   return (
-    <header>
-      <a className="title" href="/">
+    <header class={styles.header}>
+      <a class={styles.title} href="/">
         blog.takurinton.dev
       </a>
     </header>
