d77c7f2a38fc145cb3f0f2819a5aff6c375dcd7f
takurinton <takurinton@takurinton.dev>
2023-10-21 09:32:30 +0900

    maintenance
    feedf
    
    fix
    
    sentense
    
    remove prerender
    
    fix lint file
    
    update packages
    
    remove dependabot
    
    ...

--- public/types.d.ts
+++
@@ -1,5 +0,0 @@
-declare type GlobalNodeModule = {
-  hot?: {
-    accept: (cb: (u: any) => void) => void;
-  };
-} & NodeModule;
--- wmr.config.ts
+++
@@ -1,8 +0,0 @@
-export default function () {
-  return {
-    alias: {
-      "src/*": "src",
-    },
-    // customRoutes: ["/rss.xml"],
-  };
-}
