5449c15f9ba8e61388d4423a9ea8eb304ac10481
takurinton <takurinton@takurinton.dev>
2024-07-10 21:30:31 +0900

    fix test

--- app/tests/lib.rs
+++ app/tests/lib.rs
@@ -7,14 +7,45 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_render() {
+    fn test_render_brace() {
         let html = markdown_to_html("# hello world");
-        let input = render! {
-            <div></div>
-        }
-        .to_string();
+        assert_eq!(render!({ html }).to_string(), "<h1>hello world</h1>\n");
+    }
 
-        let expected = "<div class=\"foo\" id=\"app\"><div><h1>hello world</h1></div><div><p>hoge</p></div></div>";
-        assert_eq!(input, expected);
+    #[test]
+    fn test_render_text() {
+        assert_eq!(
+            render! {
+                foo
+            }
+            .to_string(),
+            "foo"
+        );
+    }
+
+    #[test]
+    fn test_render_tag() {
+        assert_eq!(render!(<div></div>).to_string(), "<div></div>");
+    }
+
+    #[test]
+    fn test_render_nested() {
+        assert_eq!(
+            render! {
+                <div>
+                    <h1>hello world</h1>
+                </div>
+            }
+            .to_string(),
+            "<div><h1>hello world</h1></div>"
+        );
+    }
+
+    #[test]
+    fn test_render_attribute() {
+        assert_eq!(
+            render!(<div class="foo"></div>).to_string(),
+            "<div class=\"foo\"></div>"
+        );
     }
 }
